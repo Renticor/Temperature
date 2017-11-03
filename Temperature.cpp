302658b347223e84f16dae5d703f30dfd763a29c
DESKTOP-1Q4RJLB\Renticor <samburski.leszek@gmail.com>
2017-11-03 20:06:04 +0100

    New beginning

--- Temperature.cpp
+++
@@ -1,7 +0,0 @@
-#include "Temperature.h"
-
-Temperature::Temperature(QWidget *parent)
-	: QMainWindow(parent)
-{
-	ui.setupUi(this);
-}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "Temperature.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	Temperature w;
-	w.show();
-	return a.exec();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
